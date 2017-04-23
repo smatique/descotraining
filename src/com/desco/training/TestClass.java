4e2ba5379b5d7de6338679952789c640349ad70e
Atique <smatique@gmail.com>
2017-04-23 18:02:36 +0600

    initial commit

---
+++ src/com/desco/training/TestClass.java
@@ -0,0 +1,21 @@
+package com.desco.training;
+
+public class TestClass {
+
+	public static void main(String[] args){
+		System.out.println("First Class");
+		
+		
+		int a=18;
+		int b=5;
+		int c=a/b;
+		System.out.println(c);
+		
+		a=5;
+		b=2;
+		c=3;
+		float d=++a/--b - --c/++a+b;
+		System.out.println(~20);
+	}
+	
+}
